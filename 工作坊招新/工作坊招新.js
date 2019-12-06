61d2d4d04b3859debfafaacd0c79e47a5bfdc259
刘家乐 <42764419+1161967580@users.noreply.github.com>
2019-12-06 20:34:42 +0800

    Update 工作坊招新.js

--- 工作坊招新/工作坊招新.js
+++ 工作坊招新/工作坊招新.js
@@ -21,60 +21,17 @@ function lunbo(){
 	});
 }
 
-function SME() {
-	alert("有关SME杯详情的网站还在紧张制作中，请稍候。");
+
+function appear() {
+	var list = document.getElementsByClassName('appear');
+	for (var i = 0; i <= list.length - 1; i++) {
+		var getTopnumber=list[i].getBoundingClientRect().top;
+		if (getTopnumber<=150) {
+			list[i].style.opacity = 1;
+		}
+	}
 }
 
-function appear_1() {
-	document.getElementById('WIE2').style.opacity = 1;
-	for (var i = 0; i <= 1; i++) {
-		document.getElementsByClassName('wenzi')[i].style.opacity = 1;
-	}
-}
-function appear_2() {
-	var getId = document.getElementById('part2');
-	var getTopnumber = getId.getBoundingClientRect().top;
-	if (getTopnumber<=100) {
-		for (var i = 0; i <= 3; i++) {
-			document.getElementsByClassName('part2_0')[i].style.opacity = 1;
-		}
-	}
-}
-function appear_3() {
-	var getId = document.getElementById('part3');
-	var getTopnumber = getId.getBoundingClientRect().top;
-	if (getTopnumber<=100) {
-		for (var i = 0; i <= 3; i++) {
-			document.getElementsByClassName('jiao')[i].style.opacity = 1;
-		}
-		for (var i = 0; i <= 1; i++) {
-			document.getElementsByClassName('h2_0')[i].style.opacity = 1;
-		}
-		for (var i = 0; i <= 1; i++) {
-			document.getElementsByClassName('part3-ud')[i].style.opacity = 1;
-		}		
-	}
-}
-function appear_4() {
-	var getId = document.getElementById('part4');
-	var getTopnumber = getId.getBoundingClientRect().top;
-	if (getTopnumber<=100) {
-		document.getElementById('swiper-container').style.opacity = 1;
-	}
-}
-function appear_5() {
-	var getId = document.getElementById('part5');
-	var getTopnumber = getId.getBoundingClientRect().top;
-	if (getTopnumber<=100) {
-		document.getElementById('QA').style.opacity = 1;
-		for (var i = 0; i <= 3; i++) {
-			document.getElementsByClassName('mengxin')[i].style.opacity = 1;
-		}
-		for (var i = 0; i <= 3; i++) {
-			document.getElementsByClassName('dalao')[i].style.opacity = 1;
-		}
-	}
-}
 function appear_6() {
 	var getId = document.getElementById('part6');
 	var getBottomnumber = getId.getBoundingClientRect().bottom;
@@ -86,21 +43,33 @@ function appear_6() {
 		for (var i = 0; i <= 1; i++) {
 			document.getElementsByClassName('neirong')[i].style.opacity = 1;
 		}
+		document.getElementsByClassName('erweima')[0].style.opacity = 1;
 	}
 }
 
 function part_height() {
 	var part = ["part1","part3","part4-2"];
 	var h=window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;
+	var h4 = document.getElementsByClassName('appear_4')[0].offsetHeight;
+	console.log('1');console.log(h4);
 	for (var i = 0; i <= part.length - 1; i++) {
 		var p = part[i];
 		document.getElementById(p).style.height = h + 'px';
 	}
+	var h4 = document.getElementsByClassName('appear_4')[0].offsetHeight;
+	console.log('2');console.log(h4);
 	var w = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
 	if (w>=900) {
 		document.getElementById('part2').style.height = h +'px';
-		document.getElementById('part4').style.height = h +'px';
+		if (h4>=h) {
+			document.getElementById('part4').style.height = h4 +'px';
+		}
+		else {
+			document.getElementById('part4').style.height = h +'px';
+		}
 	}
+	var h4 = document.getElementsByClassName('appear_4')[0].offsetHeight;
+	console.log('3');console.log(h4);
 	document.getElementById('WIE2').style.paddingTop = h/2 - w*0.05 + 'px';
 	document.getElementById('up-inner').style.paddingTop = h/2 - 55 + 'px';
 }
